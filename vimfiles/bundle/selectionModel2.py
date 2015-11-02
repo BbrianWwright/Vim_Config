67e33036b3d3e336dea9817715ba75854e943d95
Brian Wright <bwright5@vt.edu>
2015-11-01 22:56:38 -0800

    oops

--- vimfiles/bundle/selectionModel2.py
+++
@@ -1,9 +0,0 @@
-
-class DogglesSelectionModel:
-    '''
-    '''
-
-    def __init__(self, parent_doggles_widget):
-        self._doggles_Widget = parent_doggles_widget
-
-        self._selected_indices
